4d68cabd09dc02d96a70868e22a7aa3a41361a67
Shirley Quelali <shirley.quelali@gmail.com>
2024-07-27 19:34:49 -0400

    integration of item on lane

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import Item from "./components/item/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Item />
+    <App />
   </React.StrictMode>
 );
