4884b55e492a765bcabf4c6498ee3a5e0ab12f26
Shirley Quelali <shirley.quelali@gmail.com>
2024-08-02 23:58:27 -0400

    functions to drag item

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,11 +1,38 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, PropsWithChildren } from "react";
 
-interface ItemProps extends HTMLAttributes<HTMLDivElement>  {
- 
-}
+type ItemProps = HTMLAttributes<HTMLDivElement> & {
+  data: any;
+  onDragStart?: (index: number) => void;
+};
 
-export const Item: FC<ItemProps> = ({ className, children, ...otras }) => {
-  return <div className={`container ${className}`} {...otras} draggable>
-    {children}
-  </div>;
+export const Item: FC<PropsWithChildren<ItemProps>> = ({
+  className,
+  children,
+  onDragStart,
+  data,
+  ...props
+}) => {
+  return (
+    <div
+      onDragStart={(e) => {
+        //console.log("Empezo", data.title)
+        e.dataTransfer.effectAllowed = "move";
+        e.dataTransfer.setData("lane/data", data.id.toString());
+      }}
+      onDragOver={(e) => {
+        e.preventDefault();
+        //console.log("sobre", data.title)
+      }}
+      onDrop={(e) => {
+        //console.log("Solto", data.title)
+        const id = parseInt(e.dataTransfer.getData("lane/data"), 10);
+        //onDropHandle(id, data.id)
+      }}
+      className={`container ${className}`}
+      {...props}
+      draggable
+    >
+      {children}
+    </div>
+  );
 };
