1b0b7fd891c4e57cb583dab86f9d7d11e8404171
Shirley Quelali <shirley.quelali@gmail.com>
2024-08-08 23:58:17 -0400

    item dragable

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,51 @@ import { Item } from "./components/item";
 import { useState } from "react";
 
 function App() {
+  const [lanes, setLanes] = useState<Lane[]>(DATA);
 
-  const [lanes, setLanes] = useState<Lane[]>(DATA)
-
-  const handleDropLane = (sourceId: number, targetId: number) => {
-    let copyLanes: Lane[] = JSON.parse(JSON.stringify(lanes));
-    let indexTarget=-1;
-    let indexSource=-1;
+  /**const handleDropLane = (sourceId: number, targetId: number) => {
+    const copyLanes: Lane[] = JSON.parse(JSON.stringify(lanes));
+    let indexTarget = -1;
+    let indexSource = -1;
     copyLanes.forEach((lane, index) => {
-      if(lane.id === sourceId){
+      if (lane.id === sourceId) {
         indexSource = index;
       }
-      if(lane.id === targetId){
+      if (lane.id === targetId) {
         indexTarget = index;
       }
-    })
-    let sourceTemp = copyLanes[indexSource];
+    });
+    const sourceTemp = copyLanes[indexSource];
     copyLanes[indexSource] = copyLanes[indexTarget];
     copyLanes[indexTarget] = sourceTemp;
     setLanes(copyLanes);
-  }
+  };*/
+
+  const handleDropLane = (itemId: number, targetLaneId: number) => {
+    const copyLanes: Lane[] = JSON.parse(JSON.stringify(lanes));
+    let sourceLaneIndex = -1;
+    let targetLaneIndex = -1;
+    let itemToMove: Item | null = null;
+
+    // Encontrar los carriles fuente y destino, y extraer el ítem que se va a mover
+    copyLanes.forEach((lane, index) => {
+      if (lane.items.some((item) => item.id === itemId)) {
+        sourceLaneIndex = index;
+        itemToMove = lane.items.find((item) => item.id === itemId) || null;
+        lane.items = lane.items.filter((item) => item.id !== itemId);
+      }
+      if (lane.id === targetLaneId) {
+        targetLaneIndex = index;
+      }
+    });
+
+    if (itemToMove && targetLaneIndex !== -1) {
+      // Insertar el ítem en la nueva posición en el carril de destino
+      copyLanes[targetLaneIndex].items.push(itemToMove);
+    }
+
+    setLanes(copyLanes);
+  };
 
   return (
     <>
@@ -34,17 +59,20 @@ function App() {
       <Panel>
         {lanes.map((lane, index) => {
           return (
-            <Lane
-              key={index}
-              data={lane}
-              onDropHandle={handleDropLane}
-            >
-              <LaneHeader>
-                {lane.title}
-              </LaneHeader>
+            <Lane key={index} data={lane} onDropHandle={handleDropLane}>
+              <LaneHeader>{lane.title}</LaneHeader>
               <LaneContent>
                 {lane.items.map((item, index) => {
-                  return <Item key={index}>{item.details}</Item>;
+                  return (
+                    <Item
+                      key={index}
+                      onDragStart={(e) => {
+                        e.dataTransfer.setData("item/id", item.id.toString());
+                      }}
+                    >
+                      {item.details}
+                    </Item>
+                  );
                 })}
               </LaneContent>
               <LaneFooter />
@@ -76,11 +104,11 @@ const DATA: Lane[] = [
     items: [
       {
         id: 1,
-        details: "Shirley",
+        details: "Shirley 1 ",
       },
       {
         id: 2,
-        details: "Edson",
+        details: "Edson 1",
       },
     ],
   },
@@ -90,11 +118,11 @@ const DATA: Lane[] = [
     items: [
       {
         id: 3,
-        details: "Shirley",
+        details: "Shirley 2",
       },
       {
         id: 4,
-        details: "Edson",
+        details: "Edson 2",
       },
     ],
   },
@@ -104,11 +132,11 @@ const DATA: Lane[] = [
     items: [
       {
         id: 5,
-        details: "Shirley",
+        details: "Shirley 3",
       },
       {
         id: 6,
-        details: "Edson",
+        details: "Edson 3",
       },
     ],
   },
--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,37 +1,14 @@
 import { FC, HTMLAttributes, PropsWithChildren } from "react";
 
-type ItemProps = HTMLAttributes<HTMLDivElement> & {
-  data: any;
-  onDragStart?: (index: number) => void;
-};
+type ItemProps = HTMLAttributes<HTMLDivElement> & {};
 
 export const Item: FC<PropsWithChildren<ItemProps>> = ({
   className,
   children,
-  onDragStart,
-  data,
   ...props
 }) => {
   return (
-    <div
-      onDragStart={(e) => {
-        //console.log("Empezo", data.title)
-        e.dataTransfer.effectAllowed = "move";
-        e.dataTransfer.setData("lane/data", data.id.toString());
-      }}
-      onDragOver={(e) => {
-        e.preventDefault();
-        //console.log("sobre", data.title)
-      }}
-      onDrop={(e) => {
-        //console.log("Solto", data.title)
-        const id = parseInt(e.dataTransfer.getData("lane/data"), 10);
-        //onDropHandle(id, data.id)
-      }}
-      className={`container ${className}`}
-      {...props}
-      draggable
-    >
+    <div className={`container ${className}`} {...props} draggable>
       {children}
     </div>
   );
